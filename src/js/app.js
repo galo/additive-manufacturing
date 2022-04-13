01a4e8442e973eaaeaf1901091aade11b5ba94bf
galo <galo@hp.com>
2022-04-13 15:43:34 -0600

    tutorial done

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,22 @@ class main {
     constructor() {
         // Instantiate two viewers for two different views
 
+        const mainViewer = new Communicator.WebViewer({
+            containerId: "viewer",
+            empty: true
+        });
+        const overheadViewer = new Communicator.WebViewer({
+            containerId: "subviewer",
+            empty: true
+        });
+ 
+        // Set class properties
+        this._viewerList = [mainViewer, overheadViewer];
+        this._modelList = [];
+
+        // Start viewers
+        this._viewerList.map( (viewer) => viewer.start() );
+
         // Set up viewers
         this.setViewerCallbacks();
         this.configureOperators();
@@ -18,6 +34,29 @@ class main {
     }  // End Constructor
 
     setViewerCallbacks() {
+        let mainViewer = this._viewerList[0];
+        let overheadViewer = this._viewerList[1];
+
+        this._viewerList.map( (viewer) => { 
+ 
+            viewer.setCallbacks({ 
+           
+              modelStructureReady: () => { 
+                // Create Printing Plane 
+                // Load Model 
+                this.loadModel("microengine", viewer);
+
+              }, 
+           
+              sceneReady: () => { 
+                // Set the cameras for the two viewers
+                // Background color for viewers 
+              } 
+           
+            }); // End Callbacks on Both Viewers 
+            
+          }); // End map
+
 
     } // End setting viewer callbacks
 
@@ -45,7 +84,20 @@ class main {
 
     // Function to load models and translate them so they are loaded 
     // at the origin and above the printing plane
-    loadModel(modelName, viewer) {
+    loadModel(modelName, viewer) { 
+        const modelNum = viewer.model.getNodeChildren(viewer.model.getAbsoluteRootNode()).length;
+        const nodeName = "Model-" + (modelNum + 1);
+        const modelNodeId = viewer.model.createNode(null, nodeName);
+        this._modelList.push(modelName);
+        viewer.model.loadSubtreeFromScsFile(modelNodeId, "./data/" + modelName + ".scs")
+            .then(() => {
+                let loadMatrix = viewer.model.getNodeNetMatrix(modelNodeId);
+                viewer.model.getNodeRealBounding(modelNodeId)
+                    .then((box) => {
+                        loadMatrix.setTranslationComponent(box.min.x * -1, box.min.y * -1, box.min.z * -1);
+                        viewer.model.setNodeMatrix(modelNodeId, loadMatrix, true);
+                });
+        });
 
     }
 
